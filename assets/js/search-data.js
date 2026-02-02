e4087031159878c782e4b39fbd79b8f8441c14b3
meruila <76788054+meruila@users.noreply.github.com>
2026-02-02 12:36:35 +0000

    Deploying to gh-pages from @ meruila/meruila.github.io@eb70e86e2ab72dd0f76531676e2f7b78572307ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,17 +61,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-unconditional",
-          title: 'Unconditional',
-          description: "presented at ESGS Indie Fiesta 2019",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+          section: "News",},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -82,30 +72,40 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{id: "projects-bsseq-tools",
-          title: 'BSSeq Tools',
+          title: 'bsseq tools',
           description: "performance evaluation of bioinformatic tools for bisulfite sequencing in animal methylomes",
           section: "Projects",handler: () => {
               window.location.href = "/projects/bioinfo_bsseq/";
             },},{id: "projects-tawilis-osmoregulatory-organs",
-          title: 'Tawilis Osmoregulatory Organs',
-          description: "PAG-ASIA 2024 submission",
+          title: 'tawilis osmoregulatory organs',
+          description: "partial outputs of methylome analysis of tawilis gills and kidneys for PAG-ASIA 2024",
           section: "Projects",handler: () => {
               window.location.href = "/projects/bioinfo_osmoregulatory/";
             },},{id: "projects-stickleback-methylation",
-          title: 'Stickleback Methylation',
+          title: 'stickleback methylation',
           description: "DNA methylation profiling of marine and freshwater stickleback",
           section: "Projects",handler: () => {
               window.location.href = "/projects/bioinfo_stickleback/";
             },},{id: "projects-tawilis-notebook",
-          title: 'Tawilis Notebook',
+          title: 'tawilis notebook',
           description: "documentation of bioinformatics processes for the tawilis multi-omics project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/bioinfo_tawilis/";
+            },},{id: "projects-tiny-game-collection",
+          title: 'tiny game collection',
+          description: "itch.io works",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/gamedev_itchio/";
             },},{id: "projects-lumina",
-          title: 'Lumina',
+          title: 'lumina',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/gamedev_lumina/";
+            },},{id: "projects-unconditional",
+          title: 'unconditional',
+          description: "presented at ESGS Indie Fiesta 2019",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/gamedev_unconditional/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
