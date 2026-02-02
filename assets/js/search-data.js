0f1a0a236653dccef0c5cbfecf76a1c9e8679591
meruila <76788054+meruila@users.noreply.github.com>
2026-02-02 14:09:26 +0000

    Deploying to gh-pages from @ meruila/meruila.github.io@ffa34be972f5693ca84264c80e26247e5249e222 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -86,9 +86,9 @@ ninja.data = [{
           description: "student record verification system",
           section: "Projects",handler: () => {
               window.location.href = "/projects/class_shacker/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-shacker",
+          title: 'SHACker',
+          description: "student record verification system",
           section: "Projects",handler: () => {
               window.location.href = "/projects/class_tja/";
             },},{id: "projects-tiny-game-collection",
