f6b3d83e62ce290803ece6becb4e4800c6e9326a
meruila <76788054+meruila@users.noreply.github.com>
2026-02-02 12:59:32 +0000

    Deploying to gh-pages from @ meruila/meruila.github.io@fa34cfbf24c4156be8e1f6ea31d3968f171ecef9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "Some projects I made.",
+          description: "list of projects I made. some of the projects here were collaborations (e.g. tawilis team, class projects).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
